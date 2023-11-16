64e6c7d2396e9cf3d6701d88f15a5798377b15ed
tantarus <76399256+tantarusss@users.noreply.github.com>
2023-11-16 15:09:34 +0100

    Update prozentrechner.py

--- OOP-Cel/prozentrechner.py
+++ OOP-Cel/prozentrechner.py
@@ -13,9 +13,9 @@ def checkInput(userInput:str, betrag):
     else:
         try:
             (operator, aufschlag) = (userInput[0], userInput[1:])
-            if operator == "+" and 1 <= int(aufschlag) <= 100:
+            if operator == "+":
                 return betrag + betrag * (int(aufschlag) * 0.01)
-            elif operator == "-" and 1 <= int(aufschlag) <= 100:
+            elif operator == "-":
                 return betrag - betrag * (int(aufschlag) * 0.01)
             else:
                 raise ValueError
